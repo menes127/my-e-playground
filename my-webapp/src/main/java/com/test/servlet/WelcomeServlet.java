a9f6c9dbbe275c678e56177e9d060dbb26511a8f
menes127 <menes127@e61f3f89-c814-7bd7-d4dd-f3d4b684ded0>
2012-05-06 12:09:12 +0000

    add doPut doDelete method

--- my-webapp/src/main/java/com/test/servlet/WelcomeServlet.java
+++ my-webapp/src/main/java/com/test/servlet/WelcomeServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	private String greeting;
 
 	public void init() {
@@ -30,12 +31,29 @@ public class WelcomeServlet extends HttpServlet {
 		out.println("</title></head>");
 		out.println("<body>");
 		out.println(welcomeInfo);
+		out.println("doGet");
 		out.println("</body></html>");
 		out.close();
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		doGet(req, resp);
+		PrintWriter out = resp.getWriter();
+		out.println("doPost");
+		out.close();
+	}
+	
+	public void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("doDelete");
+		out.close();
+	}
+	
+	public void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("doPut");
+		out.close();
 	}
 }
